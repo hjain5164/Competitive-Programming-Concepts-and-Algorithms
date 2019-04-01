3ecc51c4533cfef6b9f289dc392c23f4212957cc
Harsh Jain <hjain5164@gmail.com>
2019-04-01 09:59:00 +0530

    Topological Sort using BFS - Kahn's Algorithm

---
+++ Graphs/topological-sort-bfs.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <map>
+#include <queue>
+#include <list>
+using namespace std;
+
+template <typename T>
+class Graph
+{
+    map<T, list<T>> adjList;
+
+  public:
+    Graph()
+    {
+    }
+
+    void addEdge(T u, T v, bool bidir = true)
+    {
+        adjList[u].push_back(v);
+        if (bidir)
+            adjList[v].push_back(u);
+    }
+
+    void topologicalSort()
+    {
+        queue<T> q;
+        map<T, bool> visited;
+        map<T, int> indegree;
+        for (auto i : adjList)
+        {
+            T node = i.first;
+            visited[node] = false;
+            indegree[node] = 0;
+        }
+
+        //Init the indegrees of all nodes
+        for (auto i : adjList)
+        {
+            T u = i.first;
+            for (T v : adjList[u])
+            {
+                indegree[v]++;
+            }
+        }
+
+        //Find all nodes with 0 indegree
+        for (auto i : adjList)
+        {
+            T node = i.first;
+            if (indegree[node] == 0)
+                q.push(node);
+        }
+
+        //Start algorithm
+        while (!q.empty())
+        {
+            T node = q.front();
+            q.pop();
+            cout << node << "-->";
+            for (T neighbor : adjList[node])
+            {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0)
+                    q.push(neighbor);
+            }
+        }
+    }
+};
+int main()
+{
+    Graph<string> g;
+    g.addEdge("English", "Programming Logic", false);
+    g.addEdge("Maths", "Programming Logic", false);
+    g.addEdge("Programming Logic", "HTML", false);
+    g.addEdge("Programming Logic", "Python", false);
+    g.addEdge("Programming Logic", "Java", false);
+    g.addEdge("Programming Logic", "JS", false);
+    g.addEdge("Python", "Web Dev", false);
+    g.addEdge("HTML", "CSS", false);
+    g.addEdge("CSS", "JS", false);
+    g.addEdge("JS", "Web Dev", false);
+    g.addEdge("Java", "Web Dev", false);
+    g.addEdge("Python", "Web Dev", false);
+
+    g.topologicalSort();
+}
